de56b1298d69bae308e9386fc3939307e820c725
Anakin Yuen <anakinyuen1024@gmail.com>
2020-07-12 23:08:33 +0800

    :on: update: use ClockPage as set-arrive page and set-leave page

---
+++ src/pages/set-leave.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ClockPage from 'src/components/pages/ClockPage';
+
+const SetArrivePage: React.FC = () => <ClockPage mode="leave" />;
+
+export default SetArrivePage;
